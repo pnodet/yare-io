9565b72feac50b7285c7e9d39b7342b536ad6ee1
pnxdxt <paul.nodet@gmail.com>
2022-02-06 17:21:50 +0100

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,11 @@
-import RenderService from '../node_modules/yare-code-sync/client/RenderService';
-import {
-	chooseSpiritType,
-	shouldSelfDefend,
-	shouldDefendBase,
-	betterShouldRetreat as shouldRetreat,
-} from './common/decisions';
-import soldierStrategy from './soldier/strategies';
-import workerStrategy from './worker/strategies';
-import commonActions from './common/actions';
-
-RenderService.circle(base.position, 200, '#ff0000');
-RenderService.circle(base.position, 400, '#ff0000');
-RenderService.circle(enemy_base.position, 400, '#ff0000');
+let moveOrders: Position[] = [];
+let energizeOrders: Entity[] = [];
 
 /**
- * Iterate over each spirit and decide what to do
+ * What's more important ?
+ * - Protect the base
+ * - Kill the enemy
+ * - Retreat
+ * - Heal
+ * - Farm
  */
-for (const [index, spirit] of my_spirits.entries()) {
-	if (!memory.spirits[spirit.id]) memory.spirits[spirit.id] = {};
-	if (!spirit.type) spirit.type = chooseSpiritType();
-
-	if (shouldRetreat(spirit)) {
-		commonActions.retreat(spirit);
-		continue;
-	}
-
-	if (shouldSelfDefend(spirit)) {
-		commonActions.selfDefense(spirit);
-		continue;
-	}
-
-	if (shouldDefendBase(spirit, index)) {
-		commonActions.defendBase(spirit);
-		continue;
-	}
-
-	if (spirit.type === 'worker') {
-		workerStrategy(spirit, index);
-		continue;
-	}
-
-	if (spirit.type === 'soldier') {
-		soldierStrategy(spirit);
-		continue;
-	}
-}
